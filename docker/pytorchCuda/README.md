0ca215358a55f61d3d00bfed2d35f9df12f2eb87
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-12-28 12:43:26 +0000

    fix start-server file add docker

---
+++ docker/pytorchCuda/README.md
@@ -0,0 +1,12 @@
+# Llama.cpp Dockerfile
+
+- start-server.sh is the script to run within a pytorch-cuda container.
+- Dockerfile is the full install.
+
+## Test curl request to runpod
+```
+curl --request POST \
+    --url https://{pod-id}-8080.proxy.runpod.net/completion \
+    --header "Content-Type: application/json" \
+    --data '{"prompt": "Building a website can be done in 10 simple steps:","n_predict": 128}'
+``` 
---
+++ docker/pytorchCuda/start-server.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Clone the llama.cpp repository
+git clone https://github.com/ggerganov/llama.cpp
+
+# Change directory to llama.cpp
+cd llama.cpp || exit
+
+# Build the project with make
+make LLAMA_CUBLAS=1
+
+# Initialize variables
+gguf_file_name=""
+repo_slug=""
+context_length=""
+
+# Parse named command-line arguments
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --gguf_file-name) gguf_file_name="$2"; shift 2;;
+    --repo-slug) repo_slug="$2"; shift 2;;
+    --context-length) context_length="$2"; shift 2;;
+    *) echo "Unknown parameter passed: $1"; exit 1;;
+  esac
+done
+
+# Check if variables are set
+if [ -z "$gguf_file_name" ] || [ -z "$repo_slug" ] || [ -z "$context_length" ]; then
+    echo "Error: Missing required arguments"
+    echo "Usage: $0 --gguf_file-name <file_name> --repo-slug <repo_slug> --context-length <context_length>"
+    exit 1
+fi
+
+# Assemble the model path
+model_path="${repo_slug}/resolve/main/${gguf_file_name}"
+
+# Check if the file exists and download if it doesn't
+if [ ! -f "/llama.cpp/${gguf_file_name}" ]; then
+    wget -P /llama.cpp "https://huggingface.co/${model_path}"
+else
+    echo "${gguf_file_name} already exists!"
+fi
+
+# Build the command with the -ngl flag
+command="/llama.cpp/server -m /llama.cpp/${gguf_file_name} -c ${context_length} --port 8080 --host 0.0.0.0 -ngl 64"
+
+# Execute the command in the background
+$command &
+
+# Optionally print a message indicating that the server has started
+echo "Server started..."
