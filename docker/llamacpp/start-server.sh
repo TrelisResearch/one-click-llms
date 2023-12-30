b8db207c22f33b9d6e2fa10a294ce80e97d0b7ef
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-12-30 11:32:59 +0000

    support reviisons

--- docker/llamacpp/start-server.sh
+++ docker/llamacpp/start-server.sh
@@ -4,6 +4,7 @@
 gguf_file_name=""
 repo_slug=""
 context_length=""
+revision="main"  # Default to the main branch
 num_parallel_threads=16  # Default to 16 parallel threads
 model_dir="${MODEL_DIR:-/workspace}"  # Use the environment variable or default to /workspace
 
@@ -14,6 +15,7 @@ while [ "$#" -gt 0 ]; do
     --repo-slug) repo_slug="$2"; shift 2;;
     --context-length) context_length="$2"; shift 2;;
     --np) num_parallel_threads="$2"; shift 2;;
+    --revision) revision="$2"; shift 2;;
     *) echo "Unknown parameter passed: $1"; exit 1;;
   esac
 done
@@ -21,7 +23,7 @@ done
 # Check if variables are set
 if [ -z "$gguf_file_name" ] || [ -z "$repo_slug" ] || [ -z "$context_length" ]; then
     echo "Error: Missing required arguments"
-    echo "Usage: $0 --gguf-file-name <file_name> --repo-slug <repo_slug> --context-length <context_length> [--np <num_parallel_threads>]"
+    echo "Usage: $0 --gguf-file-name <file_name> --repo-slug <repo_slug> --context-length <context_length> [--np <num_parallel_threads>] [--revision <revision>]"
     exit 1
 fi
 
@@ -34,8 +36,9 @@ download_model() {
     local repo_id="${1}"
     local file_name="${2}"
     local token="${3:-}"
+    local revision="${4:-main}"
 
-    python -c "from huggingface_hub import hf_hub_download; hf_hub_download(repo_id='${repo_id}', filename='${file_name}', token='${token}' if token else None)"
+    python -c "from huggingface_hub import hf_hub_download; hf_hub_download(repo_id='${repo_id}', filename='${file_name}', revision='${revision}', token='${token}' if token else None)"
 }
 
 cd ${model_dir}
@@ -50,12 +53,12 @@ make LLAMA_CUBLAS=1
 cd ../
 
 # Assemble the model path
-model_path="${repo_slug}/resolve/main/${gguf_file_name}"
+model_path="${repo_slug}/resolve/${revision}/${gguf_file_name}"
 
 # Check if the model file exists in the model directory and download if it doesn't
 if [ ! -f "${model_dir}/${gguf_file_name}" ]; then
     echo "Model file not found in ${model_dir}. Downloading..."
-    download_model "${repo_slug}" "${gguf_file_name}" "${HUGGING_FACE_HUB_TOKEN}"
+    download_model "${repo_slug}" "${gguf_file_name}" "${HUGGING_FACE_HUB_TOKEN}" "${revision}"
     echo "Download completed."
 else
     echo "Model file ${gguf_file_name} already exists in ${model_dir}"
